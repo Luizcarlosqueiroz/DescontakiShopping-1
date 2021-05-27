9ceefc29bb28bd717edbf2ed3b90600b7a4dca71
Thiago Mateus <thiagomateus123@hotmail.com>
2021-05-27 12:30:20 -0300

    Visual da página home feito (visual temporário)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,7 @@ import './index.css'
 import relatorioImg from '../../assets/images/lojista.png'
 import consumidorImg from '../../assets/images/consumidor.png'
 import gestaoDeCuponsImg from '../../assets/images/percentage.png'
+import registrationImg from '../../assets/images/registration.png'
 import MenuLateral from '../../components/MenuLateral'
 
 import NavigationButton from '../../components/NavigationButton'
@@ -24,7 +25,7 @@ const Menu = ()=>{
                 <div id='shoppingOptions'>
                     <Link to="/relatorios"><NavigationButton value='Relatório dos Lojistas' imgSrc={relatorioImg}/></Link>
                     <Link to="/relatorios"><NavigationButton value='Relatório dos Consumidores' imgSrc={consumidorImg}/></Link>
-                    <Link to="/cadastrarlojista"><NavigationButton value='Cadastrar Lojista' imgSrc={consumidorImg}/></Link>
+                    <Link to="/cadastrarlojista"><NavigationButton value='Cadastrar Lojista' imgSrc={registrationImg}/></Link>
                     <Link to="/gestaodecupons"><NavigationButton value='Gestão de Cupons' imgSrc={gestaoDeCuponsImg}/></Link>
                 </div>
                 <hr className='hrLarge'></hr>
