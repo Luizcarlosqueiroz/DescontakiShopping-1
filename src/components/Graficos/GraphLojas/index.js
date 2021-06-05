66cea8480eb49313de21f46572c3060faa238246
Luiz Carlos Queiroz <57020720+Luizcarlosqueiroz@users.noreply.github.com>
2021-06-05 17:33:14 -0300

    Desenvolvimento Pedro
    Adicionando;
    - Login
    - Cadastro
    Alterando:
    - Menu Lateral
    - Routes

--- src/components/Graficos/GraphLojas/index.js
+++ src/components/Graficos/GraphLojas/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Chart from "react-google-charts";
 
-import Information from "../../info-json-relatorio1";
+import Information from "../../data/info-json-relatorio1";
 
 const GraphLojas = () => {
 
